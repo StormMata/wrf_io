d7f8a23f589db585b0aed6426b1bf6d6a66e8afd
StormMata <storm.mata097@gmail.com>
2024-12-11 22:06:56 -0500

    updates for anvil

--- wrf_io/preproc.py
+++ wrf_io/preproc.py
@@ -521,8 +521,7 @@ def summary_table(namelist: Namelist, turbine: Turbine, opt_params: Dict[str, An
     turbine_center_x = namelist.i_parent_start * namelist.outer_dx + turbine.turb_x
 
     # Define the console for rich output
-    # console = Console(record=True)
-    console = Console()
+    console = Console(record=True)
 
     # Create a rich table
     table = Table()
@@ -683,30 +682,7 @@ def summary_table(namelist: Namelist, turbine: Turbine, opt_params: Dict[str, An
     #########################################
 
     # Export the table to plain text
-    # table_text = console.export_text(styles=False)  # Optionally remove styles for plain text
-
-    # Suppress output to the terminal
-    buffer = io.StringIO()  # Create a buffer
-    original_file = console.file  # Save the original console output stream
-
-    try:
-        # Redirect console output to the buffer
-        console.file = buffer
-        console.print(table)  # Render the table (output goes to the buffer)
-    finally:
-        # Restore the original console output stream
-        console.file = original_file
-
-    # Retrieve the table as plain text
-    table_text = buffer.getvalue()
-
-    # Export the table to plain text
-    # from io import StringIO
-    # buffer = StringIO()
-    # console.file = buffer  # Redirect console output to the buffer
-    # console.print(table)  # Render the table to the buffer
-    # table_text = buffer.getvalue()  # Get the content of the buffer
-    # buffer.close()  # Close the buffer
+    table_text = console.export_text(styles=False)  # Optionally remove styles for plain text
 
     # Define the output filename
     output_filename = opt_params['save_to'] + "/summary.txt"
